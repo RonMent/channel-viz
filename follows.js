ccd6fc25d3ccda76d36d1fb71527981790c724b5
Ron <ronmentink@gmail.com>
2014-03-13 12:02:30 +0100

    Update follows.js
    Api Key en Feed toegevoegd

--- follows.js
+++ follows.js
@@ -12,9 +12,9 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
+	var defaultKey		= '0qXRU8hqeEayZIWj5m8h6xHpUqTxPPsne15SzIbo73iOhJw6', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [948809414], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'RonEnergieHub', // Replaces Xively logo in the header
 		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
